fdcc71ad1c69f8650d11660fc09c98a9ff201fa2
shailesh_334 <shaileshgurle334@gmail.com>
2025-07-14 22:55:45 +0530

    configured routing and error page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom/client";
 
 import Header from "./components/Header";
 import Body from "./components/Body";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
+import {  createBrowserRouter, RouterProvider } from "react-router";
+
 
 const Applayout = () => {
   return (
@@ -13,5 +18,23 @@ const Applayout = () => {
   );
 };
 
+const appRouter = createBrowserRouter([
+  {
+    path:"/",
+    element:<Applayout/>,
+    errorElement:<Error/>,
+  },
+   {
+    path:"/about",
+    element:<About/>
+  },
+  {
+    path:"/contact",
+    element:<Contact/>
+  },
+]);
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Applayout/>);
+
+root.render(<RouterProvider router={appRouter}/>);
